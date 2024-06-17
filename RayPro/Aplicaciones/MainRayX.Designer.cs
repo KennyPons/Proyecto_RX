9456c66d84a0e5285759bea1644a2fdce199ede5
Christian Eduardo Taipe Cueva <christianecueva@gmail.com>
2024-06-17 11:49:36 -0500

    elimina cuadro de cambio manual de valores y el main acomodacion mejor hora y fecha

--- RayPro/Aplicaciones/MainRayX.Designer.cs
+++ RayPro/Aplicaciones/MainRayX.Designer.cs
@@ -37,6 +37,7 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.lblEncender = new System.Windows.Forms.Label();
             this.lblHora = new System.Windows.Forms.Label();
             this.lblFecha = new System.Windows.Forms.Label();
             this.btnON = new System.Windows.Forms.PictureBox();
@@ -69,7 +70,6 @@
             this.txtProyeccion = new System.Windows.Forms.TextBox();
             this.txtEstructura = new System.Windows.Forms.TextBox();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.lblEncender = new System.Windows.Forms.Label();
             this.btnMinimizar = new System.Windows.Forms.PictureBox();
             this.btnClose = new System.Windows.Forms.PictureBox();
             this.imageLista = new System.Windows.Forms.ImageList(this.components);
@@ -210,12 +210,23 @@
             this.pictureBox3.TabIndex = 1;
             this.pictureBox3.TabStop = false;
             // 
+            // lblEncender
+            // 
+            this.lblEncender.AutoSize = true;
+            this.lblEncender.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEncender.ForeColor = System.Drawing.Color.Brown;
+            this.lblEncender.Location = new System.Drawing.Point(307, 43);
+            this.lblEncender.Name = "lblEncender";
+            this.lblEncender.Size = new System.Drawing.Size(68, 33);
+            this.lblEncender.TabIndex = 14;
+            this.lblEncender.Text = "OFF";
+            // 
             // lblHora
             // 
             this.lblHora.AutoSize = true;
             this.lblHora.Font = new System.Drawing.Font("Century Gothic", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblHora.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(100)))), ((int)(((byte)(180)))));
-            this.lblHora.Location = new System.Drawing.Point(618, 73);
+            this.lblHora.Location = new System.Drawing.Point(622, 73);
             this.lblHora.Name = "lblHora";
             this.lblHora.Size = new System.Drawing.Size(107, 33);
             this.lblHora.TabIndex = 15;
@@ -226,7 +237,7 @@
             this.lblFecha.AutoSize = true;
             this.lblFecha.Font = new System.Drawing.Font("Century Gothic", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblFecha.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.lblFecha.Location = new System.Drawing.Point(639, 130);
+            this.lblFecha.Location = new System.Drawing.Point(621, 120);
             this.lblFecha.Name = "lblFecha";
             this.lblFecha.Size = new System.Drawing.Size(86, 23);
             this.lblFecha.TabIndex = 14;
@@ -265,7 +276,7 @@
             this.btnR.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnR.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
             this.btnR.Image = global::RayPro.Properties.Resources.btnReseteo__1;
-            this.btnR.Location = new System.Drawing.Point(718, 729);
+            this.btnR.Location = new System.Drawing.Point(718, 732);
             this.btnR.Name = "btnR";
             this.btnR.Size = new System.Drawing.Size(146, 119);
             this.btnR.TabIndex = 11;
@@ -282,7 +293,7 @@
             this.btnRX.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnRX.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
             this.btnRX.Image = global::RayPro.Properties.Resources.btnRX__1;
-            this.btnRX.Location = new System.Drawing.Point(439, 729);
+            this.btnRX.Location = new System.Drawing.Point(439, 730);
             this.btnRX.Name = "btnRX";
             this.btnRX.Size = new System.Drawing.Size(146, 119);
             this.btnRX.TabIndex = 10;
@@ -298,7 +309,7 @@
             this.btnPRE.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnPRE.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
             this.btnPRE.Image = global::RayPro.Properties.Resources.btnPre__1;
-            this.btnPRE.Location = new System.Drawing.Point(147, 739);
+            this.btnPRE.Location = new System.Drawing.Point(147, 732);
             this.btnPRE.Name = "btnPRE";
             this.btnPRE.Size = new System.Drawing.Size(146, 119);
             this.btnPRE.TabIndex = 3;
@@ -309,11 +320,11 @@
             // 
             this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Tomato;
-            this.label3.Location = new System.Drawing.Point(786, 863);
+            this.label3.Location = new System.Drawing.Point(779, 863);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(21, 19);
+            this.label3.Size = new System.Drawing.Size(27, 25);
             this.label3.TabIndex = 9;
             this.label3.Text = "R";
             // 
@@ -321,11 +332,11 @@
             // 
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Tomato;
-            this.label2.Location = new System.Drawing.Point(498, 863);
+            this.label2.Location = new System.Drawing.Point(491, 860);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(32, 19);
+            this.label2.Size = new System.Drawing.Size(41, 25);
             this.label2.TabIndex = 8;
             this.label2.Text = "RX";
             // 
@@ -333,11 +344,11 @@
             // 
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Tomato;
-            this.label1.Location = new System.Drawing.Point(198, 866);
+            this.label1.Location = new System.Drawing.Point(195, 860);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 19);
+            this.label1.Size = new System.Drawing.Size(54, 25);
             this.label1.TabIndex = 7;
             this.label1.Text = "PRE";
             // 
@@ -345,9 +356,9 @@
             // 
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Image = global::RayPro.Properties.Resources.IconMedic;
-            this.pictureBox1.Location = new System.Drawing.Point(835, 73);
+            this.pictureBox1.Location = new System.Drawing.Point(831, 73);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 56);
+            this.pictureBox1.Size = new System.Drawing.Size(150, 70);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
@@ -432,11 +443,11 @@
             // 
             this.textFilamento.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.textFilamento.AutoSize = true;
-            this.textFilamento.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textFilamento.Font = new System.Drawing.Font("Tahoma", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textFilamento.ForeColor = System.Drawing.Color.Tomato;
-            this.textFilamento.Location = new System.Drawing.Point(454, 335);
+            this.textFilamento.Location = new System.Drawing.Point(435, 279);
             this.textFilamento.Name = "textFilamento";
-            this.textFilamento.Size = new System.Drawing.Size(70, 29);
+            this.textFilamento.Size = new System.Drawing.Size(83, 35);
             this.textFilamento.TabIndex = 14;
             this.textFilamento.Text = "Foco";
             // 
@@ -649,17 +660,6 @@
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(1064, 40);
             this.panel2.TabIndex = 0;
-            // 
-            // lblEncender
-            // 
-            this.lblEncender.AutoSize = true;
-            this.lblEncender.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEncender.ForeColor = System.Drawing.Color.Brown;
-            this.lblEncender.Location = new System.Drawing.Point(307, 43);
-            this.lblEncender.Name = "lblEncender";
-            this.lblEncender.Size = new System.Drawing.Size(68, 33);
-            this.lblEncender.TabIndex = 14;
-            this.lblEncender.Text = "OFF";
             // 
             // btnMinimizar
             // 
