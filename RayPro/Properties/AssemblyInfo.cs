0030a7d43b0a562b84bdb8db26574a557fc4021a
Christian Eduardo Taipe Cueva <christianecueva@gmail.com>
2024-06-19 02:37:41 -0500

    guardar cambio de agregar icono

--- RayPro/Properties/AssemblyInfo.cs
+++ RayPro/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
 [assembly: AssemblyTitle("RayPro")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Aplicacion de escritotio para cotrol de rayos X")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ELVIMEDIC")]
 [assembly: AssemblyProduct("RayPro")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
